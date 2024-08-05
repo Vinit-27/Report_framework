79feb3842345fdc844426eede164b413ed6453d3
Vinit Parmar <121330087+Vinit-27@users.noreply.github.com>
2024-08-05 12:24:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,20 @@ let data= fetch('https://run.mocky.io/v3/69f60a58-3a36-48c5-a9cf-b100b015950c').
         return eval(data.slice(data.indexOf('[')));
       });
 
-var dataset = [];
+let table = document.getElementById("Employee");
+let row;
 data.then(value =>{
     for (let i=0;i<value.length;i++){
-        dataset.push([value[i]['id'],value[i]['name'],value[i]['age'],value[i]['role'],value[i]['hireDate'],value[i]['isActive'],value[i]['salary'],value[i]['department'],value[i]['projectCompleted'],value[i]['accessLevel']]);
+        row= table.insertRow(i+1);
+        row.insertCell(0).innerHTML=value[i]['id'];
+        row.insertCell(1).innerHTML=value[i]['name'];
+        row.insertCell(2).innerHTML=value[i]['age'];
+        row.insertCell(3).innerHTML=value[i]['role'];
+        row.insertCell(4).innerHTML=value[i]['hireDate'];
+        row.insertCell(5).innerHTML=value[i]['isActive'];
+        row.insertCell(6).innerHTML=value[i]['salary'];
+        row.insertCell(7).innerHTML=value[i]['department'];
+        row.insertCell(8).innerHTML=value[i]['projectCompleted'];
+        row.insertCell(9).innerHTML=value[i]['accessLevel'];
     }
 })
-var table = document.querySelector('#Employee')
-table.innerHTML = '<tr><td>' + dataset + '</td></tr>'
