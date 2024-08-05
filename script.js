65778a88eea1b790c1841fa567f687064cbfc66a
Vinit Parmar <121330087+Vinit-27@users.noreply.github.com>
2024-08-05 12:00:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,11 @@ let data= fetch('https://run.mocky.io/v3/69f60a58-3a36-48c5-a9cf-b100b015950c').
         return eval(data.slice(data.indexOf('[')));
       });
 
-console.log(data)
+var dataset = [];
+data.then(value =>{
+    for (let i=0;i<value.length;i++){
+        dataset.push([value[i]['id'],value[i]['name'],value[i]['age'],value[i]['role'],value[i]['hireDate'],value[i]['isActive'],value[i]['salary'],value[i]['department'],value[i]['projectCompleted'],value[i]['accessLevel']]);
+    }
+})
+var table = document.querySelector('#Employee')
+table.innerHTML = '<tr><td>' + dataset + '</td></tr>'
