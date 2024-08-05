a4f39a348326b7290d474e9f1d16a996f7b5c34a
Vinit Parmar <121330087+Vinit-27@users.noreply.github.com>
2024-08-05 11:14:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-data=fetch('https://run.mocky.io/v3/69f60a58-3a36-48c5-a9cf-b100b015950c',get)
-.then(response => {
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  return response.json();
-})
-.then(data => {
-  console.log('Data received:', data);
-})
-.catch(error => {
-  console.error('There was a problem with the fetch operation:', error);
-});
+
+let data= fetch('https://run.mocky.io/v3/69f60a58-3a36-48c5-a9cf-b100b015950c').then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(data=>{
+        return eval(data.slice(data.indexOf('[')));
+      });
+
+console.log(data)
